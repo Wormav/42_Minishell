a78b95fcadb904e7d5a4fb84d05ebcc3fe9b90ad
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-01-11 15:09:44 +0100

    clean main

--- src/main.c
+++ src/main.c
@@ -6,38 +6,37 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 09:37:21 by stetrel           #+#    #+#             */
-/*   Updated: 2025/01/11 13:13:07 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/01/11 15:09:29 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	main(int argc, char **argv)
+static void	process(char *argv1)
 {
-	char		*str;
 	static int	err = 0;
 	t_token		*list;
 	t_ast		*ast;
 
-	if (argc != 2)
-		return (1);
-	str = ft_strtrim(argv[1], " ");
 	ast = NULL;
-	list = token_parse_string(str);
+	list = token_parse_string(argv1);
 	check_unsupported_char(list, &err);
 	if (err)
-		parser_identify_error(err, list, str);
+		parser_identify_error(err, list, argv1);
 	err = parser_check(list);
 	if (err)
-		token_identify_error(err, list, str);
-	//print_token_list(list);
+		token_identify_error(err, list, argv1);
 	list = parser_identify(list);
 	parser_define_priority(&list);
 	parser_join_tokens(list);
 	ast = ast_create(list, ast);
-	print_tree(ast);
-	//print_token_list(list);
-	ast_free(ast);
-	free_token(list);
-	free(str);
+	//print_tree(ast);
+	clean_memory(ast, list, argv1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (1);
+	process(ft_strtrim(argv[1], " "));
 }
