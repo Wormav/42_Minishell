092894bfb39a12c230ee44282f57c230c9879958
Jeremy lorette <jeremy.lorette@outlook.com>
2025-01-14 20:44:22 +0100

    pwd implementation

--- includes/utils.h
+++ includes/utils.h
@@ -6,16 +6,16 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 14:29:24 by jlorette          #+#    #+#             */
-/*   Updated: 2025/01/11 14:38:48 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/01/14 20:39:12 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-#include <token.h>
-#include <ast.h>
+# include <token.h>
+# include <ast.h>
 
 void	clean_memory(t_ast *ast, t_token *token, char *str);
 
-# endif
+#endif
