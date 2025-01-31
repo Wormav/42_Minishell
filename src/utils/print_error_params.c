85fdafee2893e801f1da483b97cbfff61e9f68c8
Jeremy lorette <jeremy.lorette@outlook.com>
2025-01-31 18:13:57 +0100

    norme

--- src/utils/print_error_params.c
+++ src/utils/print_error_params.c
@@ -6,56 +6,56 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:01:05 by jlorette          #+#    #+#             */
-/*   Updated: 2025/01/31 17:45:41 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/01/31 18:10:19 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static int check_quotes(const char *str, int *i)
+static int	check_quotes(const char *str, int *i)
 {
-    char quote;
-    int only_spaces;
-    int space_count;
+	char	quote;
+	int		only_spaces;
+	int		space_count;
 
-    quote = str[*i];
-    only_spaces = 1;
-    space_count = 0;
-    (*i)++;
-    while (str[*i] && str[*i] != quote)
-    {
-        if (str[*i] != ' ')
-            only_spaces = 0;
-        if (str[*i] == ' ')
-            space_count++;
-        (*i)++;
-    }
-    if (only_spaces)
-        return (space_count);
-    if (str[*i])
-        (*i)++;
-    return (0);
+	quote = str[*i];
+	only_spaces = 1;
+	space_count = 0;
+	(*i)++;
+	while (str[*i] && str[*i] != quote)
+	{
+		if (str[*i] != ' ')
+			only_spaces = 0;
+		if (str[*i] == ' ')
+			space_count++;
+		(*i)++;
+	}
+	if (only_spaces)
+		return (space_count);
+	if (str[*i])
+		(*i)++;
+	return (0);
 }
 
-static int is_invalid_params(const char *str)
+static int	is_invalid_params(const char *str)
 {
-    int i;
-    int space_count;
+	int	i;
+	int	space_count;
 
-    i = 0;
-    space_count = 0;
-    while (str && str[i])
-    {
-        if (str[i] == '\'' || str[i] == '"')
-        {
-            space_count = check_quotes(str, &i);
-            if (space_count)
-                return (space_count);
-        }
-        else
-            i++;
-    }
-    return (0);
+	i = 0;
+	space_count = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			space_count = check_quotes(str, &i);
+			if (space_count)
+				return (space_count);
+		}
+		else
+			i++;
+	}
+	return (0);
 }
 
 char	*create_string_spaces_error(int nbr_spaces)
@@ -74,7 +74,7 @@ char	*create_string_spaces_error(int nbr_spaces)
 	return (error);
 }
 
-int print_error_params(t_cmd *cmd, long *error)
+int	print_error_params(t_cmd *cmd, long *error)
 {
 	int		error_code;
 	char	*string_error;
@@ -87,7 +87,8 @@ int print_error_params(t_cmd *cmd, long *error)
 	else
 	{
 		string_error = create_string_spaces_error(error_code);
-		printf("%s: cannot access '%s': No such file or directory\n", cmd->cmd, string_error);
+		printf("%s: cannot access '%s': No such file or directory\n",
+			cmd->cmd, string_error);
 		lp_free(string_error);
 	}
 	*error = 2;
