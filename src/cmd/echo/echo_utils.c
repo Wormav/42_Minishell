672174099cb8cf98593162cf1fc9bd5f299374f8
Jeremy lorette <jeremy.lorette@outlook.com>
2025-02-01 17:22:58 +0100

    fix

--- src/cmd/echo/echo_utils.c
+++ src/cmd/echo/echo_utils.c
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:17:21 by jlorette          #+#    #+#             */
-/*   Updated: 2025/01/31 18:06:01 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/02/01 17:22:19 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	echo_check_dollar_sign(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '$' && ft_isalnum(str[i + 1]))
+		if (str[i] == '$' && (ft_isalnum(str[i + 1]) || str[i + 1] == '?'))
 			return (1);
 		i++;
 	}
