79730c39cd0f0212d91bae15684e4da9c9fd910f
Jeremy lorette <jeremy.lorette@outlook.com>
2025-02-13 18:40:13 +0100

    odd quotes

--- src/errors/error_odd_quotes.c
+++ src/errors/error_odd_quotes.c
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 17:55:03 by jlorette          #+#    #+#             */
-/*   Updated: 2025/01/20 18:29:21 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/02/13 18:35:26 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 void	check_odd_quotes(char *str, int *err)
 {
-	int	quotes;
-	int	dquotes;
-	int	i;
+    int	quotes;
+    int	dquotes;
+    int	i;
 
-	quotes = 0;
-	dquotes = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '"' && !quotes)
-			dquotes = !dquotes;
-		else if (str[i] == '\'' && !dquotes)
-			quotes = !quotes;
-		i++;
-	}
-	*err = (quotes != 0 || dquotes != 0);
+    quotes = 0;
+    dquotes = 0;
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == '"' && !quotes)
+            dquotes = !dquotes;
+        else if (str[i] == '\'' && !dquotes)
+            quotes = !quotes;
+        i++;
+    }
+    if (quotes)
+        *err = ERR_ODD_QUOTE;
+    else if (dquotes)
+        *err = ERR_ODD_DQUOTE;
 }
--- src/errors/token_errors.c
+++ src/errors/token_errors.c
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 08:40:13 by stetrel           #+#    #+#             */
-/*   Updated: 2025/02/13 18:20:19 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/02/13 18:38:40 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	token_identify_error(t_data *data, t_token *lst)
 {
-	lp_free(data->str_prompt);
-	free_token(lst);
-	if (data->error == ERR_ODD_QUOTE)
+	if (data->str_prompt)
+		lp_free(data->str_prompt);
+	if (lst)
+		free_token(lst);
+	if (data->error_parsing == ERR_ODD_QUOTE)
 		write (2, "Not an odd number of quote\n", 27);
-	else if (data->error == ERR_ODD_DQUOTE)
+	else if (data->error_parsing == ERR_ODD_DQUOTE)
 		write (2, "Not an odd number of dquote\n", 28);
-	data_close_and_exit(data, 1);
 }
